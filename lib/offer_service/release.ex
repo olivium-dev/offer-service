1c40cc6eb3af655a6504bea0f4635f01d2613473
oudaykhaled <ouday.khaled@gmail.com>
2026-08-19 04:14:33 +0200

    style: satisfy offer CI formatter

--- lib/offer_service/release.ex
+++ lib/offer_service/release.ex
@@ -7,6 +7,7 @@ defmodule OfferService.Release do
 
   def migrate do
     load_app()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
